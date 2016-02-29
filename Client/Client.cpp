3d9fca53a631536204b59348502abb134d739aca
dobai <dobai.michal@gmail.com>
2016-02-29 18:08:05 +0100

    Command line argument IP parameter
    Ak je programu poskytnuty prave jeden argument, tak je interpretovany
    ako IP adresa vzdialeneho stroja. Inak je pouzity loopback (127.0.0.1)

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "stdafx.h"
 #using "system.dll"
 
@@ -9,12 +10,23 @@ using namespace System::Net::Sockets;
 
 
 
-int main()
+int main(int argc, char** argv)
 {
+	
 	//array<unsigned char>^ v = { 'a','a', 0 };
-	array<unsigned char>^ v = gcnew array<unsigned char>(1000000);
-
-	IPAddress^ address = IPAddress::Parse("127.0.0.1");
+	array<unsigned char>^ v  = gcnew array<unsigned char>(1000000);
+	IPAddress^ address;
+		
+	if(argc==2) {
+		String^ ip = gcnew String(argv[1]);
+		address = IPAddress::Parse(ip);
+		printf("%s\n", argv[1]);
+	}
+	else {
+		address = IPAddress::Parse("127.0.0.1");
+		printf("Using 127.0.0.1\n");
+	}
+	
 	IPEndPoint^ endPoint = gcnew IPEndPoint(address, 8888);
 	Socket^ socket = gcnew Socket(endPoint->AddressFamily, SocketType::Stream, ProtocolType::Tcp);
 	socket->Connect(endPoint);
--- Server/Server.cpp
+++ Server/Server.cpp
@@ -7,11 +7,25 @@ using namespace System::Net;
 using namespace System::Net::Sockets;
 
 
-int main()
+int main(int argc, char** argv)
 {
 	//array<unsigned char>^ v = {0,0,0,0,0,0,0,0,0};
 	array<unsigned char>^ v = gcnew array<unsigned char>(1000000);
-	IPAddress^ address = IPAddress::Parse("127.0.0.1");
+	
+	IPAddress^ address;
+	
+	if (argc == 2) {
+		String^ ip = gcnew String(argv[1]);
+		address = IPAddress::Parse(ip);
+		printf("%s\n", argv[1]);
+	}
+	else {
+		address = IPAddress::Parse("127.0.0.1");
+		printf("Using 127.0.0.1\n");
+	}
+	
+	
+	
 	IPEndPoint^ endPoint = gcnew IPEndPoint(address, 8888);
 	Socket^ Ssocket = gcnew Socket(endPoint->AddressFamily, SocketType::Stream, ProtocolType::Tcp);
 	Ssocket->Bind(endPoint);
